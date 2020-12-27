0d8a87e73320b7706e7ee0fecd8419a66837c891
Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>
2020-12-27 22:44:35 +0100

    Added gdb, modified others
    Signed-off-by: Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>

--- fish/config.fish
+++ fish/config.fish
@@ -5,5 +5,8 @@ set -g fish_greeting ''
 set BROWSER firefox
 
 alias gdb 'gdb -q'
+alias vgr "valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --leak-resolution=high"
+alias gccc "gcc -Wextra -Wall -Werror -std=c99 -pedantic -g -ggdb3"
+alias clang-format-10 "clang-format"
 
 starship init fish | source

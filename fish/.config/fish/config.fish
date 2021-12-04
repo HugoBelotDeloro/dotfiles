4d973117f8b042d8af5a7cd9977a48491dddef87
Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>
2021-12-04 23:45:20 +0100

    kitty conf and made kitty default terminal
    Signed-off-by: Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -54,4 +54,25 @@ function tmux-help
     "Other key bindings should be default"
 end
 
+function kitty-help
+    echo "Font:"
+    printf "\t%s\n" \
+    "C-S-minus: decrease font size"\
+    "C-S-plus: increase font size"
+    echo "Tabs:"
+    printf "\t%s\n" \
+    "C-S-t: new tab"\
+    "C-S-q: close tab"\
+    "C-S-left: previous tab"\
+    "C-S-right: next tab"\
+    "C-S-,: move tab left"\
+    "C-S-;: move tab right"
+    echo "Windows:"
+    printf "\t%s\n"\
+    "C-S-enter: new window"\
+    "C-S-w: close window"\
+    "C-S-up: previous window"\
+    "C-S-down: next window"
+end
+
 starship init fish | source
